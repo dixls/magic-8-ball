206299872e8434c69b2facecbd2d3bf6f2162cb4
pixls <tha.pixls@gmail.com>
2022-08-22 14:38:27 -0400

    cleaned up styling, fixed issue with backgroundcolor

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,17 +4,17 @@ import "./EightBall.css";
 const EightBall = ({answers}) => {
     const [fortune, setFortune] = useState("Think of a Question");
     const [color, setColor] = useState("black");
-    console.log(answers)
     const chooseAnswer = () => {
         let idx = Math.floor(Math.random() * answers.length);
         let answer = answers[idx];
         setFortune(answer.msg);
         setColor(answer.color);
+        console.log(answer);
     }
 
     return (
-        <div className="eightBall" style={{backgroundColor: "${color}"}} onClick={chooseAnswer}>
-            <span>{fortune}</span>
+        <div className="eightBall" style={{backgroundColor: color}} onClick={chooseAnswer}>
+            <p>{fortune}</p>
         </div>
     )
 }
